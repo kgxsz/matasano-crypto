bfa10ae86f855f79618c420c4576e51f3a823443
Keigo Suzukawa <k.suzukawa@gmail.com>
2017-10-02 20:41:52 +1100

    Update use of readers and writers in core

--- src/matasano_crypto/core.clj
+++ src/matasano_crypto/core.clj
@@ -1,15 +1,17 @@
 (ns matasano-crypto.core
-  (:require [matasano-crypto.utils :as utils]))
+  (:require [matasano-crypto.readers :as readers]
+            [matasano-crypto.writers :as writers]
+            [matasano-crypto.utils :as utils]))
 
 
 (defn challenge-one
   [s]
-  (utils/write-base64-string (utils/read-even-hex-string s)))
+  (writers/write-base64-string (readers/read-hex-string s)))
 
 
 (defn challenge-two
   [s1 s2]
-  (utils/write-hex-string (utils/fixed-XOR (utils/read-even-hex-string s1) (utils/read-even-hex-string s2))))
+  (writers/write-hex-string (utils/fixed-XOR (readers/read-hex-string s1) (readers/read-hex-string s2))))
 
 
 (defn challenge-three
@@ -25,6 +27,28 @@
          (first)
          (:plaintext))))
 
+
 (defn challenge-five
   [k s]
   (utils/encrypt-with-repeating-XOR-cipher k s))
+
+
+#_(defn challenge-six
+  []
+  (let [url "http://cryptopals.com/static/challenge-data/6.txt"
+        bs (utils/read-base64-string (clojure.string/replace (slurp url) #"\n" ""))
+        key-sizes (range 2 41)
+        block-analysis (for [key-size key-sizes]
+                         (let [blocks (take (* 2 key-size) bs)
+                               block-a (take key-size blocks)
+                               block-b (drop key-size blocks)
+                               hamming-distance (float (/ (utils/hamming-distance (byte-array block-a)
+                                                                                  (byte-array block-b))
+                                                          key-size))]
+                           {:key-size key-size :hamming-distance hamming-distance}))
+        most-likey-key-size (:key-size (first (sort-by :hamming-distance block-analysis)))]
+    (for [i (range most-likey-key-size)]
+      (keep #(nth % i nil) (partition-all most-likey-key-size bs)))))
+
+#_(challenge-six)
+
